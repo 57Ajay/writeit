d88b143396c3dbbb6efa4143469a6b44d3ecd37c
57Ajay <57ajay.u@gmail.com>
2024-10-05 12:09:28 +0530

    !0

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import userRoute from './routes/user.route'
 import { prismaClientMiddleware } from './middlewares/auth.middleware';
 import blogRouter from './routes/blog.route';
 
 const app = new Hono();
 
+app.use("*", cors({
+  origin: '*',
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+})
+);
 app.use("*", prismaClientMiddleware)
 
 app.route("/api/v1/user", userRoute);
