b4ba3afd354f4a5347c84bfffccbaa79c74df958
57Ajay <57ajay.u@gmail.com>
2024-10-16 15:14:34 +0530

    cors fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,19 +6,30 @@ import blogRouter from './routes/blog.route';
 
 const app = new Hono();
 
+const allowedOrigins = [
+  'https://storyarc.underroot-1.online',
+  'https://writeit-aac8p0v24-57ajayu-gmailcoms-projects.vercel.app'
+];
+
 app.use("*", cors({
-  origin: '*',
+  origin: (origin) => {
+    if (allowedOrigins.includes(origin)) {
+      return origin;
+    }
+    return null;
+  },
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowHeaders: ['Content-Type', 'Authorization'],
-})
-);
+}));
+
+
 app.use("*", prismaClientMiddleware)
 
 app.route("/api/v1/user", userRoute);
 app.route("/api/v1/blog", blogRouter);
 
 app.get("/", (c) => {
-  return c.text("THis is medium application")
-})
+  return c.text("THis is blog application backend, developed by Ajay Upadhyay")
+});
 
 export default app
